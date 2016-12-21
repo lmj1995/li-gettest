5915f23eed7692e99b4610641d1d7679c35d36f5
lmj1995 <1169019701@qq.com>
2016-12-21 11:28:55 +0800

    Delete lmj.sql

--- data base/lmj.sql
+++
@@ -1,9 +0,0 @@
-create database haha;
-  use haha;
-  create table user(
-  hobby varchar(20),
-  name varchar(20),
-  age int
-   );
-   insert into user(hobby,name,age)
-   values('eat','zhang','19');
