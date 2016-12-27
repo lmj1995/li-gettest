2873a167ef5161aa6c32b98a8a2ef6ac443ca9ee
lmj1995 <1169019701@qq.com>
2016-12-27 09:07:18 +0800

    Update forum.sql

--- forum.sql
+++ forum.sql
@@ -6,17 +6,17 @@ create table f_board(
   date  datetime,
   description varchar(64)
 );
-insert into f_board(ID,name,date,description)
-values(1,'青葱','data-format (now(),'%y-%m-%d')','运动');
+insert into user(ID,name,date,description)
+values(1,'青葱',now(),'运动');
 
 create table f_forum(
   FID int primary  key not null,
   forum_name varchar(32),
   forum_describe varchar(128)
 );
-insert into f_forum(FID,forum_name,forum_describe)
+insert into user(FID,forum_name,forum_describe)
 values(1,'美食','我爱美食');
-insert into f_forum(FID,forum_name,forum_describe)
+insert into user(FID,forum_name,forum_describe)
 values(2,'运动','我爱运动');
 
 create table f_article(
@@ -29,9 +29,9 @@ create table f_article(
   LastReplayDate timestamp
   LastReplayUID  int
 );
-insert into f_article(AID,theme,Post_time,Browsing_times,Countent,UID,LastReplayDate,LastReplayUID)
+insert into user(AID,theme,Post_time,Browsing_times,Countent,UID,LastReplayDate,LastReplayUID)
 values(1,'运动','now','1','运动介绍','1','now','1');
-insert into f_article(AID,theme,Post_time,Browsing_times,Countent,UID,LastReplayDate,LastReplayUID)
+insert into user(AID,theme,Post_time,Browsing_times,Countent,UID,LastReplayDate,LastReplayUID)
 values(1,'美食','now','1','美食介绍','1','now','1');
 
 create table f_reply(
@@ -41,81 +41,81 @@ create table f_reply(
   UID int,
   replydate timestamp
 );
-insert into f_reply(time,Countent,Title,UID,Countent,UID,replydate)
-values('now','美食介绍','陕菜','1','now');
-insert into f_reply(time,Countent,Title,UID,Countent,UID,replydate)
-values('now','美食介绍','湘菜','2','now');
+insert into user(time,Countent,Title,UID,replydate)
+values(now(),'美食介绍','陕菜','1','now');
+insert into user(time,Countent,Title,UID,replydate)
+values(now(),'美食介绍','湘菜','2','now');
 
 create table f_moderator(
-  FID int primary key not null,
-  UID int primary key not null,
-  foreign key(FID,UID),
+  FID int  not null,
+  UID int  not null,
   OperUID int,
   Operdate datetime
 );
-insert into f_moderator(FID,UID,FID,UID,OperUID,Operdate)
-values('1','1','1','1','11','now');
-insert into f_moderator(FID,UID,FID,UID,OperUID,Operdate)
-values('2','2','2','2','22','now');
+insert into user(FID,UID,OperUID,Operdate)
+values('1','1','11','now');
+insert into user(FID,UID,OperUID,Operdate)
+values('2','2','22','now');
 
 create table f_userdesc(
-  UID int primary key not null,
-  UID int foreign key,
+  UID int  not null,
+
   desci varchar(68),
   name varchar(68),
   sex varchar(68),
   birthday datetime
 );
-insert into f_userdesc(UID,UID,desci,name,sex,birthday)
-values('1','1','爱美食','jack','男','1995.1.4');
-insert into f_userdesc(UID,UID,desci,name,sex,birthday)
-values('2','2','爱美食','mary','女','1995.1.5');
+insert into user(UID,desci,name,sex,birthday)
+values('1''爱美食','jack','男','1995-1-4');
+insert into user(UID,desci,name,sex,birthday)
+values('2','爱美食','mary','女','1995-1-5');
 
 create table f_user(
   UID int primary key not null,
   user_name varchar(64),
   password varchar(64)
 );
-insert into f_user(UID,user_name,password)
+insert into user(UID,user_name,password)
 values('1','lmj','123456l');
-insert into f_user(UID,user_name,password)
+insert into user(UID,user_name,password)
 values('2','lmj','123456l');
 
 create table f_entity7(
-  FID int primary key not null,
-  UID int primary key not null,
-  foreign key(FID,UID)
+  FID int not null,
+  UID int  not null,
+
 );
-insert into f_entity7(FID,UID,FID,UID)
-values('1','1','1','1');
-insert into f_entity7(FID,UID,FID,UID)
-values('2','2','2','2');
+insert into user(FID,UID)
+values('1','1');
+insert into user(FID,UID)
+values('2','2');
 
 
 create table f_role(
   RoleID int primary key not null,
   RoleName varchar(64)
 );
-insert into f_role(RoleID,RoleName)
+insert into user(RoleID,RoleName)
 values('1','rose');
-insert into f_role(RoleID,RoleName)
+insert into user(RoleID,RoleName)
 values('2','mark');
 
 create table f_entity9(
-  FID int primary key not null,
-  UID int primary key not null,
-  foreign key(FID,UID)
+  FID int  not null,
+  UID int  not null,
+
 );
-insert into f_entity9(FID,UID,FID,UID)
-values('1','1','1','1');
-insert into f_entity9(FID,UID,FID,UID)
-values('2','2','2','2');
+insert into user(FID,UID)
+values('1','1');
+insert into user(FID,UID)
+values('2','2');
 
 create table f_authority(
-  RID int primary key not null,
+  RoleID int primary key not null,
   RightName varchar(64)
 );
-insert into f_authority(RoleID,RoleName)
+insert into user(RoleID,RightName)
 values('1','rose');
-insert into f_authority(RoleID,RoleName)
+insert into user(RoleID,RightName)
 values('2','mark');
+;
