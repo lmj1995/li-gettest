c16a92a0fed22a9625c9f51ded232ce6f9588351
lmj1995 <1169019701@qq.com>
2017-01-03 16:38:24 +0800

    Update JUnitTest.java

--- JUnit/JUnitTest.java
+++ JUnit/JUnitTest.java
@@ -14,17 +14,17 @@ public class JUnitTest {
 	public void test() {
 		
 		JUnit junit = new JUnit();
-		
-		assertEquals( 13  , junit.add(9, 4));
-	
-		assertEquals( 5  , junit.sub(9, 4));
-		
-		assertTrue (5 < 6);
-		
-		assertFalse(5 > 6);
-		
-		assertSame(5,5);
-		
+		//两个变量和等式是否相等	
+
+	        assertEquals( 13  , junit.add(9, 4));
+                assertEquals( 5  , junit.sub(9, 4));
+		//检查条件为真		
+                assertTrue (5 < 6);
+		//检查条件为假		
+                assertFalse(5 > 6);
+		//方法检查两个相关对象是否指同一对象		
+                assertSame(5,5);
+		//方法检查两个相关对象是否不指同一对象
 		assertNotSame(3,4);
 	}
 
